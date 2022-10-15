01c98c75d2d6d76a8199f2292ac4df5355798eed
wodus <wodus.est@gmail.com>
2022-10-15 12:31:56 +0200

    first commit

---
+++ pages/[slug].js
@@ -0,0 +1,71 @@
+import Message from "../components/message";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import{auth, db} from '../utils/firebase';
+import { toast, Toast } from "react-toastify";
+import { arrayUnion, Timestamp, updateDoc, doc, getDoc } from "firebase/firestore";
+import { async } from "@firebase/util";
+
+export default function Details(){
+    const router = useRouter();
+    const routeData = router.query;
+    const [message, setMessage] = useState("");
+    const [allMessage, setAllMessages] = useState([]);
+    //submit a message
+    const submitMessage = async() => {
+        if(!auth.currentUser) return router.push('auth/login');
+
+        if(!message){
+            toast.error("Don`t leave an empty message",{
+                position: toast.POSITION.TOP_CENTER,
+                autoClose: 1500,
+            });
+            return;
+        }
+        const docRef = doc(db, 'posts', routeData.id);
+        await updateDoc(docRef, {
+            comments: arrayUnion({
+                message,
+                avatar: auth.currentUser.photoURL,
+                userName: auth.currentUser.displayName,
+                time: Timestamp.now(),
+            }),
+        });
+
+        setMessage("");
+    }
+    //Get comments
+    const getComments = async () => {
+        const docRef = doc(db, 'posts', routeData.id);
+        const docSnap = await getDoc(docRef);
+        setAllMessages(docSnap.data().comments);
+    }
+    useEffect(()=>{
+        if(!router.isReady) return;
+        getComments();
+    },[]);
+    return(
+        <div>
+            <Message {...routeData}>
+            </Message>
+            <div className="my-4">
+                <div className="flex">
+                    <input onChange={(e) => setMessage(e.target.value)} type="text" value={message} placeholder="Send a message" className="bg-gray-800 w-full p-2 text-white text-sm"/>
+                    <button onClick={submitMessage} className="bg-cyan-500 text-white py-2 px-4 text-sm">Send</button>
+                </div>
+                <div className="py-6">
+                    <h2 className="font-bold">Comments</h2>
+                    {allMessage?.map(message => (
+                        <div className="bg-white p-4 my-4 border-2" key={message.time}>
+                            <div className="flex items-center gap-2 mb-4">
+                                <img className="w-10 rounded-full" src={message.avatar}></img>
+                                <h2>{message.userName}</h2>
+                            </div>
+                            <h2>{message.message}</h2>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
